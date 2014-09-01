4aed9467ee33e9923786f26ee2d138defa8bb760
huozhi <inbox@huozhi.im>
2014-09-01 22:58:33 +0800

    chat interface

--- reveal.js
+++ reveal.js
@@ -1093,7 +1093,8 @@ var Reveal = (function(){
 					}
 					else {
 						// slide.style.top = Math.max( - ( getAbsoluteHeight( slide ) / 2 ) - slidePadding, -slideHeight / 2 ) + 'px';
-						slide.style.top = Math.max( slideHeight / 2 - getAbsoluteHeight( slide ) / 2 ) + 'px';
+						// slide.style.top = Math.max( slideHeight / 2 - getAbsoluteHeight( slide ) / 2 ) + 'px';
+						slide.style.top = 0;
 					}
 				}
 				else {
