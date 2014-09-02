b3a1500dee52fb363b542a0bb4bacb9fc93ce820
xhh261672 <563249383@qq.com>
2014-09-03 00:04:06 +0800

    fix file create bug

---
+++ impresses/imp4.md
@@ -0,0 +1,5 @@
+
+
+    #include <node.js>
+    <div id = "spliter"></div>
+
--- split.js
+++ split.js
@@ -3,15 +3,14 @@ var fs = require('fs');
 var fileString = fs.readFileSync('./example.md', 'utf-8');
 var impresses = fileString.split(/\+{6,}/);
 
-fs.exists('impresses/', function(isExist){
-	if(!isExist){
-		console.log('mkdir');
-		fs.mkdirSync('impresses/');
-	}
-});
+if(!fs.exists('impresses')){
+	fs.mkdirSync('impresses');
+}
 
 
 for (i = 0; i < impresses.length; i++){
+	fs.openSync('impresses/imp'+i+'.md', 'w');
+
 	fs.writeFile('impresses/imp'+i+'.md', impresses[i], function(err){
 		if(err) console.log(err.message);
 
