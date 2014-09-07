484582735ed22869357953c8156278eb01f14096
huozhi <inbox@huozhi.im>
2014-09-08 00:48:52 +0800

    webrtc demo setup

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,18 @@ router.get('/', function(req, res) {
 router.get('/login', function(req, res) {
   res.render('login');
 });
+
+router.get('/chat', function(req, res) {
+  res.render('chat');
+})
+
+router.get('/history', function(req, res) {
+  res.render('history');
+})
+
+// router.get('/test', function(req, res) {
+//   console.log('filter')
+//   res.send('hehe')
+// })
+
 module.exports = router;
