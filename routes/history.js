c956244a618b40523a1d6276d166dc4231295698
lsn : ) <chris_lsn@foxmail.com>
2014-09-12 11:42:04 +0800

    add send data

--- routes/history.js
+++ routes/history.js
@@ -5,7 +5,11 @@ router.get('/', function (req, res) {
   res.render('history');
 });
 
+router.get('/historyDetail', function (req, res) {
+  res.render('historyDetail');
+});
+
 router.get('/get-all-history', function (req, res) {
-  
+ 
 });
 module.exports = router;
