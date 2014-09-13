bfa13c4428b8063f867a475a828ab43b2295be67
xhh261672 <563249383@qq.com>
2014-09-13 14:16:41 +0800

    exports

---
+++ test/export.bat
@@ -0,0 +1,6 @@
+mongoexport -d ecclesia -c Images -o Images.dat
+mongoexport -d ecclesia -c Meetings -o Meetings.dat
+mongoexport -d ecclesia -c MdTemp -o MdTemp.dat
+mongoexport -d ecclesia -c Users -o Users.dat
+
+
