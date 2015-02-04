24ea5635eec9d00a8682e13381fd4c526d32281f
huozhi <inbox@huozhi.im>
2015-02-05 00:03:00 +0800

    rm old modules, completely use model proxy, but I have still not finished the design of the front-end interactivity.. WTF..

--- routes/webroutes.js
+++ routes/webroutes.js
@@ -2,16 +2,26 @@ var express = require('express');
 var router = express.Router();
 var auth = require('../controllers').auth;
 var home = require('../controllers').home;
+var history = require('../controllers').history;
+var chat = require('../controllers').chat;
 
+// index
 router.get('/', auth.index);
 router.get('/login', auth.loginView);
 router.post('/register', auth.registerAction);
 router.post('/login', auth.loginAction);
 
+// home views
 router.get('/home/', home.index);
 router.get('/logout', home.logout);
 router.post('/home/create', home.createRoom);
 router.post('/home/join', home.joinRoom);
 
+// history views
+router.get('/history', history.index);
+router.get('/history/detail/:room/:host/:date', history.getDiscussDetail);
+
+// chat views
+router.get('/chat', chat.index);
 
 module.exports = router;
