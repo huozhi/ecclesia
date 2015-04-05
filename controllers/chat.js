3aeb6aba4c2a1575d14e87e301a0b37107001623
lavende <gilesliu12@gmail.com>
2015-04-06 00:53:23 +0800

    add save impress, chart logic code

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
-var Discuss = require('../proxy').Discuss;
-var User = require('../proxy').User;
-var Topic = require('../proxy').Topic;
+var DiscussProxy = require('../proxy').Discuss;
+var UserProxy = require('../proxy').User;
+var TopicProxy = require('../proxy').Topic;
 var ChartModel = require('../models').Chart;
+var TopicModel = require('../models').Topic;
 
 exports.test = function (req, res, next) {
   return res.render('test', {});
@@ -19,24 +20,34 @@ exports.index = function (req, res, next) {
 exports.upload = function (req, res, next) {
   var type = req.params.type;
   var title = req.body.title;
-  if (type === 'chart') {
-    var chart = new ChartModel();
-    chart.labels = req.body.labels;
-    chart.values = req.body.values;
-    console.log(chart);
-  }
-  else if (type === 'impress') {
-
-    var impress = req.files.impress;
-    console.log(impress.path);
-    fs.readFile(impress.path, 'utf-8', function (err, content) {
-      if (err) { res.send(false); return next(); }
-      console.log(content);
-      return res.send(true);
-    });
-    
-  }
-  // then save to the discuss corrosponding to the specific title
+  var topic;
+  TopicProxy.findByTitle(title, function (err, result) {
+    if (err) {
+      topic = new TopicModel();
+      topic.title = title;
+    }
+    else {
+      topic = result;
+    }
+    if (type === 'chart') {
+      var chart = new ChartModel();
+      chart.labels = req.body.labels;
+      chart.values = req.body.values;
+      topic.chart.push(chart);
+      console.log(chart);
+    }
+    else if (type === 'impress') {
+      var impress = req.files.impress;
+      topic.impress.push(impress);
+      console.log(impress.path);
+      fs.readFile(impress.path, 'utf-8', function (err, content) {
+        if (err) { res.send(false); return next(); }
+        console.log(content);
+        return res.send(true);
+      });
+    }
+    topic.save();
+  });
 };
 
 
