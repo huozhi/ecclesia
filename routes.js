20f0c53573e28c83521b7607e62f52b003450ad5
huozhi <inbox@huozhi.im>
2015-09-26 01:51:10 +0800

    chat page reconstruct

--- routes.js
+++ routes.js
@@ -1,34 +1,34 @@
-var express = require('express');
-var router = express.Router();
-var auth = require('./controllers').auth;
-var home = require('./controllers').home;
-var history = require('./controllers').history;
-var chat = require('./controllers').chat;
+var express = require('express')
+var router = express.Router()
+var auth = require('./controllers').auth
+var home = require('./controllers').home
+var history = require('./controllers').history
+var chat = require('./controllers').chat
 
 // index
-router.get('/', auth.index);
-router.get('/login', auth.loginView);
-router.post('/register', auth.registerAction);
-router.post('/login', auth.loginAction);
+router.get('/', auth.index)
+router.get('/login', auth.loginView)
+router.post('/register', auth.registerAction)
+router.post('/login', auth.loginAction)
 
 // home views
-router.get('/home/', home.index);
-router.get('/logout', home.logout);
-router.post('/home/create', home.createRoom);
-router.post('/home/join', home.joinRoom);
+router.get('/home/', home.index)
+router.get('/logout', home.logout)
+router.post('/home/create', home.createRoom)
+router.post('/home/join', home.joinRoom)
 
 // history views
-router.get('/history', history.index);
-router.get('/history/detail', history.getDiscussDetail);
+router.get('/history', history.index)
+router.get('/history/detail', history.getDiscussDetail)
 
 // chat views
-router.get('/test', chat.test);
-router.get('/chat', chat.index);
-router.post('/chat/upload/:type', chat.upload);
+router.get('/chat', chat.index)
+router.post('/chat', chat.sync)
+router.post('/chat/upload/:type', chat.upload)
 
 
-// router.post('/query-img', );
-// router.post('/room/outline', );
+// router.post('/query-img', )
+// router.post('/room/outline', )
 
 
-module.exports = router;
+module.exports = router
