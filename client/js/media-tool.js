17fbf0c5b2f250b9fcb5646dfed5f79f13ff26bc
huozhi <inbox@huozhi.im>
2018-06-17 22:45:15 +0800

    enhance: use postcss import, rewrite media-tool

--- client/js/media-tool.js
+++ client/js/media-tool.js
@@ -1,6 +1,8 @@
+import { resolve } from "url";
+
 'use strict'
 
-const Ready = function(asrc, vsrc, mready, cready) {
+const MediaReady = function(asrc, vsrc, mready, cready) {
   this.audioSource = asrc   || null,
   this.videoSource = vsrc   || null;
   this.micReady    = mready || false,
@@ -8,74 +10,40 @@ const Ready = function(asrc, vsrc, mready, cready) {
 }
 
 const MediaTool = {
-  check: function(callback) {
-    var mr = new Ready(),
+  check: function() {
+    const mr = new MediaReady(),
         audioList  = [],
-        cameraList = [];
+        videoList = [];
 
-    window.navigator.mediaDevices.enumerateDevices().then((sourceInfos) => {
-      for (let i = 0; i < sourceInfos.length; i++) {
-        var sourceInfo = sourceInfos[i];
-        if (sourceInfo.kind === 'audioinput') {
-          audioList.push({id: sourceInfo.id, label: sourceInfo.label || 'microphone-' + i});
-        } else if (sourceInfo.kind === 'videoinput') {
-          cameraList.push({id: sourceInfo.id, label: sourceInfo.label || 'camera-' + i});
-        } else {
+    const promise = window.navigator.mediaDevices.enumerateDevices()
+
+    return promise.then((mediaDeviceInfos) => {
+      const sources = 
+        mediaDeviceInfos.filter(
+          (mdi, index, arr) => arr.findIndex(a => a.groupId === mdi.groupId) === index
+        )
+      for (let i = 0; i < sources.length; i++) {
+        const source = sources[i];
+        if (source.kind === 'audioinput') {
+          audioList.push({id: source.groupId, label: source.label || 'microphone-' + i});
+        } else if (source.kind === 'videoinput') {
+          videoList.push({id: source.groupId, label: source.label || 'camera-' + i});
         }
+
         if (audioList.length) {
           mr.audioSource = audioList[0].id;
-          $(this).data('audioSource', audioList[0].id);
           mr.micReady = true;
-          $('#select-audio').text(audioList[0].label)
         }
-        if (cameraList.length) {
-          mr.videoSource = cameraList[0].id;
-          $(this).data('videoSource', cameraList[0].id);
+        if (videoList.length) {
+          mr.videoSource = videoList[0].id;
           mr.camReady = true;
-          $('#select-camera').text(cameraList[0].label)
         }
       }
-      var cameraMenu = $('.camera-menu');
-      cameraList.forEach(function (ele, index) {
-        var cameraChoice = '<li role="presentation"><p class="camera-device-id sr-only">' +
-        ele.id + '</p><a class="camera-item" role="menuitem" tabindex="-1">' + ele.label + '</a></li>';
-        cameraMenu.append(cameraChoice);
-      });
+      mr.videoList = videoList
+      mr.audioList = audioList
 
-      var audioMenu = $('.audio-menu');
-      audioList.forEach(function (ele) {
-        var audioChoice = '<li role="presentation">' + '<p class="audio-device-id sr-only">' +
-        ele.id + '</p><a class="audio-item" role="menuitem" tabindex="-1">' + ele.label + '</a></li>';
-        audioMenu.append(audioChoice);
-      });
-
-      $('a.audio-item').click(function() {
-        mr.audioSource = $(this).prev().text();
-        $(this).data('audioSource', $(this).prev().text());
-        $('#select-audio').text($(this).text())
-      });
-
-      $('a.camera-item').click(function() {
-        mr.videoSource = $(this).prev().text();
-        $(this).data('videoSource', $(this).prev().text());
-        $('#select-camera').text($(this).text())
-      });
-
-      $('.media-check-close').click(function() {
-        audioMenu.children().remove();
-        cameraMenu.children().remove();
-      });
-
-      window.constraints = {
-        audio: {
-          optional: [{sourceId: $(this).data('audioSource')}]
-        },
-        video: {
-          optional: [{sourceId: $(this).data('videoSource')}]
-        }
-      };
-      if (typeof callback === 'function') return callback(window.constraints);
-    });
+      return mr
+    })
   }
 }
 
